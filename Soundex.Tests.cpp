9bdf1bdb75cf97f15cbb0fea58648314e822ca71
KevalBavadiya <60056875+KevalBavadiya@users.noreply.github.com>
2024-09-01 16:12:01 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -2,11 +2,34 @@
 #include "Soundex.h"
 
 
-TEST(SoundexTest, HandlesEmptyString) {
+TEST(SoundexTest, BasicTest) {
+    EXPECT_EQ(generateSoundex("Singh"), "S520");
+    EXPECT_EQ(generateSoundex("Kumar"), "K560");
+    EXPECT_EQ(generateSoundex("Sharma"), "S650");
+    EXPECT_EQ(generateSoundex("Patel"), "P340");
+    EXPECT_EQ(generateSoundex("Gupta"), "G130");
+}
+
+TEST(SoundexTest, EdgeCases) {
+    EXPECT_EQ(generateSoundex("Chand"), "C530");
+    EXPECT_EQ(generateSoundex("Mehra"), "M600");
+    EXPECT_EQ(generateSoundex("Pandey"), "P530");
     EXPECT_EQ(generateSoundex(""), "");
 }
 
-TEST(SoundexTest, HandlesSingleCharacter) {
+TEST(SoundexTest, CaseInsensitive) {
+    EXPECT_EQ(generateSoundex("SINGH"), "S520");
+    EXPECT_EQ(generateSoundex("kUmAr"), "K560");
+    EXPECT_EQ(generateSoundex("shARmA"), "S650");
+}
+
+TEST(SoundexTest, SingleCharacterName) {
     EXPECT_EQ(generateSoundex("A"), "A000");
+    EXPECT_EQ(generateSoundex("B"), "B000");
+}
+
+TEST(SoundexTest, AllVowels) {
+    EXPECT_EQ(generateSoundex("Aeio"), "A000");
+    EXPECT_EQ(generateSoundex("Euio"), "E000");
 }
 
