5a59ca9843dc638c7455cb55b7580f712a285bf8
KevalBavadiya <60056875+KevalBavadiya@users.noreply.github.com>
2024-09-08 20:51:46 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -19,8 +19,8 @@ char mapToSoundexCode(char c) {
 void appendSoundex(std::string& soundex, char code, char& prevCode) {
     if (code != '0' && code != prevCode) {
         soundex += code;
-        prevCode = code;
     }
+    prevCode = code;
 }
 
 std::string paddingSoundex(const std::string& soundex) {
